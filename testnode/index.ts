f8a8d791e25ab9420e3d5aaae4e4a5d2df3ad838
Tonissonn <antoniunicolaeille@yahoo.com>
2022-07-28 14:30:49 +0300

    rest api endpoints for mails with express

--- testnode/index.ts
+++ testnode/index.ts
@@ -1,6 +1,8 @@
 import { json } from "stream/consumers";
 import * as inputMails from "./mails.json"
 import * as fs from 'fs';
+import { ServerResponse, IncomingMessage } from "http";
+import path from "path";
 
 interface Mail{
     from: string;
@@ -14,37 +16,52 @@ function addToJson(newMail: Mail){
 
     jsonData.push(newMail);
 
-    fs.writeFileSync('./add.json', JSON.stringify(jsonData, null, 4) , 'utf-8')
+    fs.writeFileSync('./mails.json', JSON.stringify(jsonData, null, 4) , 'utf-8')
+    return newMail;
 }
 
 function deleteFromJson(index: number){
     let oldData: string = fs.readFileSync("./mails.json", "utf8")
     let jsonData: Mail[] = JSON.parse(oldData)
 
-    if (index > -1) {
+    if (index > -1 && index < jsonData.length) {
         jsonData.splice(index, 1);
+        fs.writeFileSync('./mails.json', JSON.stringify(jsonData, null, 4) , 'utf-8')
+        return true;
     }
-
-    fs.writeFileSync('./del.json', JSON.stringify(jsonData, null, 4) , 'utf-8')
+    return false;
 }
 
 function updateToJson(newMail: Mail, pos: number){
     let oldData: string = fs.readFileSync("./mails.json", "utf8")
     let jsonData: Mail[] = JSON.parse(oldData)
-    
-    jsonData[pos]=newMail
-
-    fs.writeFileSync('./update.json', JSON.stringify(jsonData, null, 4) , 'utf-8')
+    if (pos > -1 && pos < jsonData.length) {
+        jsonData[pos]=newMail
+        fs.writeFileSync('./mails.json', JSON.stringify(jsonData, null, 4) , 'utf-8')
+        return true;
+    }
+    return false;
 }
 
 function getOneFromJson(pos: number){
     let oldData: string = fs.readFileSync("./mails.json", "utf8")
     let jsonData: Mail[] = JSON.parse(oldData)
-
+    return jsonData[pos];
     console.log(jsonData[pos])
 }
 
-addToJson({from:"Ana",date:"acum",to:["idk"]})
-deleteFromJson(1)
-updateToJson({from:"Marina",date:"atunci",to:["noone"]}, 1)
-getOneFromJson(1)
+function getAllFromJson(){
+    let oldData: string = fs.readFileSync("./mails.json", "utf8")
+    let jsonData: Mail[] = JSON.parse(oldData)
+    return jsonData;
+    console.log(jsonData)
+}
+
+
+
+// addToJson({from:"Ana",date:"acum",to:["idk"]})
+// deleteFromJson(1)
+// updateToJson({from:"Marina",date:"atunci",to:["noone"]}, 1)
+// getOneFromJson(1)
+
+export {Mail,addToJson as addMail,deleteFromJson as delMail,updateToJson as updMail,getOneFromJson as getMail, getAllFromJson as getMails};
---
+++ testnode/server.ts
@@ -0,0 +1,66 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { addMail,getMail,getMails,delMail,updMail} from './index';
+
+const app = express();
+const PORT = 3000;
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+   extended: true
+ }));
+ app.use(bodyParser.json());
+
+ app.get('/test', (req, res) => {
+
+   res.send('RIP');
+ });
+ 
+app.get('/:id', (req, res) => {
+
+  res.send(getMail(parseInt(req.params.id)));
+});
+
+app.get('/', (req, res) => {
+
+   res.send(getMails());
+ });
+
+
+app.post('/mail', (req, res) => {
+  const { from, date, to } = req.body;
+  console.log(req.body)
+  if (!from?.trim() || !date?.trim()) {
+      console.log(from,date,to)
+    return res.status(400).send('Bad fields');
+  }
+  addMail({ from, date, to });
+  res.status(201).send('Mail added');
+});
+
+app.delete('/:id', (req, res) => {
+   let result = delMail(parseInt(req.params.id));
+   if(!result){
+      console.log("Failed to delete")
+      return res.status(400).send("Invalid index");
+   }
+   console.log("Not failed to delete")
+   res.status(201).send('Mail deleted');
+ });
+
+ app.put('/:id', (req, res) => {
+   const { from, date, to } = req.body;
+   console.log(req.body)
+   let result = updMail({ from, date, to },parseInt(req.params.id));
+   if(!result || !from?.trim() || !date?.trim()){
+      console.log("Failed to update")
+      return res.status(400).send("Invalid index");
+   }
+   res.status(201).send('Mail updated');
+ });
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server on http://localhost:${PORT}`);
+});
